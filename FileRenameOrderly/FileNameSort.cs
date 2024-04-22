a9578e27662e44f40c97501cbd58f8c5b9672ee4
ProJend <projend@outlook.com>
2024-04-22 16:56:31 +0800

    添加项目文件。

---
+++ FileRenameOrderly/FileNameSort.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace FileRenameOrderly
+{
+    /// <summary>
+    /// Sort by counting number
+    /// </summary>
+    public partial class FileNameSort : IComparer
+    {
+        //调用 Windos 的 DLL
+        [LibraryImport("Shlwapi.dll", StringMarshalling = StringMarshalling.Utf16)]
+        private static partial int StrCmpLogicalW(string param1, string param2);
+
+        //前后文件名进行比较。
+        public int Compare(object name1, object name2)
+        {
+            if (null == name1 && null == name2)
+            {
+                return 0;
+            }
+            if (null == name1)
+            {
+                return -1;
+            }
+            if (null == name2)
+            {
+                return 1;
+            }
+            return StrCmpLogicalW(name1.ToString(), name2.ToString());
+        }
+    }
+}
---
+++ FileRenameOrderly/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileRenameOrderly;
+
+Console.WriteLine("Hello, World!");
+int j = 1;
+
+string path = @"D:\Users\Admin\Desktop\胶液\毒液甘雨\";
+var TheFolder = new DirectoryInfo(path);
+var Files = TheFolder.GetFiles();
+Array.Sort(Files, new FileNameSort());
+foreach (var item in Files)
+{
+    if (new[] { ".jpg", ".jpeg", ".png", ".tif" }.Contains(item.Extension))
+    {
+        string newName = j++.ToString() + item.Extension;
+        string newPath = path + newName;
+
+        item.MoveTo(newPath);
+        Console.WriteLine(item.ToString() + "  Move to -->  " + newPath);
+    }
+    else
+        Console.WriteLine(item.ToString() + " is out of order");
+}
