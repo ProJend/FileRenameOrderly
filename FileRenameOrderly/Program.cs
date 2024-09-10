47b06b47ba8b45be203104352ef8ecdbf534a042
ProJend <projend@outlook.com>
2024-09-10 11:20:42 +0800

    Check the existing files

--- FileRenameOrderly/Program.cs
+++ FileRenameOrderly/Program.cs
@@ -5,7 +5,10 @@ int j = 1;
 bool ascending = true;
 bool descending = false;
 
-string path = @"\";
+string path = @"C:\";
+string tempPath = path + @"\TEMP";
+
+Directory.CreateDirectory(tempPath);
 var TheFolder = new DirectoryInfo(path);
 var Files = TheFolder.GetFiles();
 Array.Sort(Files, new FileNameSortBy(ascending));
@@ -14,11 +17,25 @@ foreach (var item in Files)
     if (new[] { ".jpg", ".jpeg", ".png", ".tif" }.Contains(item.Extension))
     {
         string newName = j++.ToString() + item.Extension;
-        string newPath = path + newName;
-
+        string newPath = tempPath + @"\" + newName;
         Console.WriteLine(item.ToString() + "  Move to -->  " + newPath);
         item.MoveTo(newPath);
     }
     else
         Console.WriteLine(item.ToString() + " is out of order");
 }
+
+Console.WriteLine();
+j = 1;
+
+TheFolder = new DirectoryInfo(tempPath);
+Files = TheFolder.GetFiles();
+Array.Sort(Files, new FileNameSortBy(ascending));
+foreach (var item in Files)
+{
+    string newName = j++.ToString() + item.Extension;
+    string newPath = path + @"\" + newName;
+    Console.WriteLine(newPath + "  <-- Move back  " + item.ToString());
+    item.MoveTo(newPath);
+}
+Directory.Delete(tempPath, true);
